922f53100447c93db639819ab9d197d995050e2d
Guilherme Branco Stracini <gui.branco@hotmail.com.br>
2015-03-13 20:46:08 +0000

    - Atualizado arquivos com base nos arquivos disponíveis no servidor FTP. - index.html alterador conforme [RR:2872] e [RR2873].

--- js/script.js
+++ js/script.js
@@ -33,12 +33,7 @@ $(function() {
 	//MAPAS GOOGLE
 	$('.mapaEditora').click(function(){
 		$(this).find('img').remove();
-		$(this).append('<iframe width="560" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=Rua+C%C3%B4nego+Jos%C3%A9+Norberto,+138+-+Alto+do+Ipiranga&amp;aq=&amp;sll=-23.609581,-46.614061&amp;sspn=0.017558,0.016458&amp;ie=UTF8&amp;hq=&amp;hnear=Rua+C%C3%B4nego+Jos%C3%A9+Norberto+-+Vila+Brasilio+Machado,+S%C3%A3o+Paulo&amp;ll=-23.611763,-46.610826&amp;spn=0.008779,0.008229&amp;t=m&amp;z=14&amp;output=embed"></iframe>');
-	});
-	
-	$('.mapaLogistica').click(function(){
-		$(this).find('img').remove();
-		$(this).append('<iframe width="560" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?q=Rua+Gama+Lobo,+1944+-+Alto+do+Ipiranga&amp;ie=UTF8&amp;hq=&amp;hnear=Rua+Gama+Lobo,+1944+-+Vila+Dom+Pedro+I,+S%C3%A3o+Paulo,+04117-091&amp;t=m&amp;z=14&amp;ll=-23.601106,-46.611843&amp;output=embed"></iframe>');
+		$(this).append('<iframe width="560" height="360" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3656.6430287754397!2d-46.5739886!3d-23.581260899999997!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5c3e2774a7bd%3A0xf0bc2bed8fdc51d1!2sR.+Itanha%C3%A9m%2C+527+-+Vila+Prudente%2C+S%C3%A3o+Paulo+-+SP%2C+03137-020!5e0!3m2!1spt-BR!2sbr!4v1414514830212"></iframe>');
 	});
 	//MARCAS
 	$('#carouselMarcas ul li').click(function(){
